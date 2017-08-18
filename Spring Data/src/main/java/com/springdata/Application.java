ce5c3f293651c65e495c71635f27de6a312bdbdb
mskalbania <cod634.steam@gmail.com>
2017-08-18 20:26:21 +0200

    added fully working gui

--- Spring Data/src/main/java/com/springdata/Application.java
+++ Spring Data/src/main/java/com/springdata/Application.java
@@ -4,8 +4,10 @@ import com.springdata.model.Book;
 import com.springdata.model.BookRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -19,35 +21,35 @@ public class Application {
 
     //COMMENTED WHEN PERFORMING INTEGRATION TESTS
 
-//    @Bean
-//    public CommandLineRunner demo(BookRepository repository) {
-//        return (args) -> {
-//            // save a couple of customers
-//          //  addBooks(repository);
-//            repository.save(new Book("Clean Code", "Not Specified", "123123", "PROGRAMMING"));
-//            repository.save(new Book("Thinking in Java", "Not Specified", "32123", "PROGRAMMING"));
-//            repository.save(new Book("Design Patterns", "Head First", "543123", "PROGRAMMING"));
-//            repository.save(new Book("Book4", "Author4", "ISBN4", "CAT4"));
-//            // fetch all customers
-//            log.info("Customers found with findAll():");
-//            log.info("-------------------------------");
-//            for (Book customer : repository.findAll()) {
-//                log.info(customer.toString());
-//            }
-//            log.info("");
-//
-//            // fetch an individual customer by ID
-//            Book customer = repository.findOne(1L);
-//            log.info("Customer found with findOne(1L):");
-//            log.info("--------------------------------");
-//            log.info(customer.toString());
-//            log.info("");
-//        };
-//    }
+    @Bean
+    public CommandLineRunner demo(BookRepository repository) {
+        return (args) -> {
+            // save a couple of customers
+           // addBooks(repository);
+            repository.save(new Book("Clean Code", "Not Specified", "123123", "PROGRAMMING"));
+            repository.save(new Book("Thinking in Java", "Not Specified", "32123", "PROGRAMMING"));
+            repository.save(new Book("Design Patterns", "Head First", "543123", "PROGRAMMING"));
+            repository.save(new Book("Book4", "Author4", "ISBN4", "CAT4"));
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Book customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            // fetch an individual customer by ID
+            Book customer = repository.findOne(1L);
+            log.info("Customer found with findOne(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
+        };
+    }
 
     public synchronized void addBooks(BookRepository repo) {
 
-        int threshold = 1000;
+        int threshold = 100;
         while ((threshold--) >= 0) {
             Book b = new Book("SomeTitle" + threshold, "SomeAuthor" + threshold,
                         "SomeISBN" + threshold, "SomeCategory" + threshold);
--- Spring Data/src/main/java/com/springdata/controller/ViewController.java
+++ Spring Data/src/main/java/com/springdata/controller/ViewController.java
@@ -1,15 +1,23 @@
 package com.springdata.controller;
 
+import com.springdata.model.Book;
+import com.springdata.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 
 
 @Controller
 public class ViewController {
+
+    @Autowired
+    private BookService bookService;
 
     //TODO Implement some gui
     @RequestMapping(value = "/home", method = RequestMethod.GET)
@@ -22,15 +30,43 @@ public class ViewController {
         return "add";
     }
 
-    @RequestMapping(value = "/bookAddSuccess")
-    public String showBookSuccess(@RequestParam("title") String title,
-                                  @RequestParam("author") String author,
-                                  @RequestParam("isbn") String isbn,
-                                  @RequestParam("category") String category) throws IOException {
-
-        //TODO implement call to its own REST SERVICE
-        System.out.println(title + author + isbn + category);
-
+    @RequestMapping(value = "/bookAddSuccess", method = RequestMethod.POST)
+    public String showBookSuccess(@ModelAttribute Book book, ModelMap modelMap) throws IOException {
+        bookService.create(book);
+        modelMap.addAttribute("book", book);
         return "bookAddSuccess";
+    }
+
+    @RequestMapping(value = "/update/{id}")
+    public String update(@PathVariable String id, ModelMap modelMap) {
+        Book book = bookService.getById(Long.parseLong(id));
+        modelMap.addAttribute("book", book);
+        return "update";
+    }
+
+    @RequestMapping(value = "/bookUpdateSuccess/{id}")
+    public String updateSuccess(@PathVariable String id,
+                                @ModelAttribute Book book, ModelMap modelMap) {
+
+        book.setId(Long.parseLong(id));
+        bookService.update(book);
+        Iterable<Book> books = bookService.getAll();
+        modelMap.addAttribute("books", books);
+        return "browse";
+    }
+
+    @RequestMapping(value = "/browse")
+    public String showBrowse(ModelMap modelMap) {
+        Iterable<Book> books = bookService.getAll();
+        modelMap.addAttribute("books", books);
+        return "browse";
+    }
+
+    @RequestMapping(value = "/delete/{id}")
+    public String delete(@PathVariable String id, ModelMap modelMap) throws InterruptedException {
+        bookService.delete(Long.parseLong(id));
+        Iterable<Book> books = bookService.getAll();
+        modelMap.addAttribute("books", books);
+        return "browse";
     }
 }
